3f873f014b843f3364a7bfc8f2a7c54cebdc7ff8
Darwin Bautista <daruuin@gmail.com>
2019-10-16 16:24:01 +0800

    Various benchmark bug fixes
    - Fixed computation of classifier score (mask predictions using baseline)
    - Ensure consistency of tensor types and devices
    - Compatibility with Tensorflow 2.0 and tf.keras

--- hw2/benchmark_keras.py
+++ hw2/benchmark_keras.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 import numpy as np
-import keras
-from keras import backend as K
+try:
+    from tensorflow import keras
+    from tensorflow.keras import backend as K
+except ImportError:
+    import keras
+    from keras import backend as K
 
 from classifier_keras import model as classifier
 from structural_similarity import structural_similarity as ssim
@@ -33,8 +37,7 @@ def test_model(model, x_test, y_test, batch_size=100):
     """
     rng = np.random.RandomState(0)
 
-    channel_dim = 1 if K.image_data_format() == 'channels_first' else -1
-    classifier.load_weights('mnist_cnn.h5', by_name=True)
+    classifier.load_weights('mnist_cnn.h5')
 
     baseline_score = 0
     correct_score = 0
@@ -43,12 +46,10 @@ def test_model(model, x_test, y_test, batch_size=100):
     N = len(x_test)
     assert N % batch_size == 0, 'N should be divisible by batch_size'
     num_batches = N // batch_size
-    for i in range(num_batches):
-        # Shape: (B, 28, 28, 1) or (B, 1, 28, 28) if channels_first
-        imgs_orig = np.expand_dims(x_test[batch_size*i:batch_size*(i + 1)], channel_dim)
-        # Shape: (B,)
-        labels = y_test[batch_size*i:batch_size*(i + 1)]
 
+    for i in range(num_batches):
+        imgs_orig = x_test[batch_size * i:batch_size * (i + 1)].astype(K.floatx())
+        labels = y_test[batch_size * i:batch_size * (i + 1)]
         # Create corruption masks
         masks = []
         for _ in range(batch_size):
@@ -59,28 +60,48 @@ def test_model(model, x_test, y_test, batch_size=100):
             y = rng.randint(0, 29 - s)
             mask = np.zeros(imgs_orig.shape[1:], dtype=np.bool)
             # Set mask area
-            mask[y:y + s, x:x + s, :] = True
+            mask[y:y + s, x:x + s] = True
             masks.append(mask)
         masks = np.stack(masks)
 
+        # Add channel dimension
+        channel_dim = 1 if K.image_data_format() == 'channels_first' else -1
+        imgs_orig = np.expand_dims(imgs_orig, channel_dim)
+        masks = np.expand_dims(masks, channel_dim)
+
+        # Generate corrupted versions
         imgs_corrupted = imgs_orig.copy()
-        # Draw squares
         imgs_corrupted[masks] = 1.
 
         # Generate restored images
         imgs_restored = model.predict_on_batch(imgs_corrupted)
 
-        predicted_labels_orig = classifier.predict_on_batch(_preprocess_for_classifier(imgs_orig)).argmax(axis=-1)
-        predicted_labels_restored = classifier.predict_on_batch(_preprocess_for_classifier(imgs_restored)).argmax(axis=-1)
-        # Calculate classifier score
+        predicted_labels_orig = classifier.predict_on_batch(_preprocess_for_classifier(imgs_orig)).argmax(axis=-1).astype(labels.dtype)
+        predicted_labels_restored = classifier.predict_on_batch(_preprocess_for_classifier(imgs_restored)).argmax(axis=-1).astype(labels.dtype)
+        # Calculate classifier score:
+        # baseline corresponds to the original samples which the classifier is able to correctly predict
         baseline = labels == predicted_labels_orig
-        correct = baseline == (labels == predicted_labels_restored)
+        # Since the classifier is NOT 100% accurate, we ignore the prediction results
+        # from the original samples which were misclassified by masking it using the baseline.
+        correct = (labels == predicted_labels_restored) & baseline
         baseline_score += int(baseline.sum())
         correct_score += int(correct.sum())
 
-        ssim_score += ssim(imgs_orig[~masks].squeeze(), imgs_restored[~masks].squeeze())
+        # Compute SSIM over the uncorrupted pixels
+        ssim_score += ssim(imgs_orig[~masks], imgs_restored[~masks])
 
     classifier_score = correct_score / baseline_score
     ssim_score /= num_batches
 
     print('Classifier score: {:.2f}\nSSIM score: {:.2f}'.format(100 * classifier_score, 100 * ssim_score))
+
+
+if __name__ == '__main__':
+    try:
+        from tensorflow.keras.datasets import mnist
+    except ImportError:
+        from keras.datasets import mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    model = IdentityModel()
+    x_test = x_test / 255
+    test_model(model, x_test, y_test)
--- hw2/benchmark_torch.py
+++ hw2/benchmark_torch.py
@@ -26,16 +26,16 @@ def test_model(model, dataset, batch_size=100):
     """Run the benchmarks for the given model
 
     :param model:
-    :param dataset: MNIST dataset with transform=ToTensor() only
+    :param dataset: MNIST test dataset
     :param batch_size: batch size to use for evaluation
     :return: None
     """
     rng = np.random.RandomState(0)
 
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     classifier = Net()
     classifier.load_state_dict(torch.load('mnist_cnn.pt', map_location=device))
-    # Switch to evaluation mode
+    classifier.to(device)
     classifier.eval()
 
     baseline_score = 0
@@ -45,47 +45,58 @@ def test_model(model, dataset, batch_size=100):
     N = len(dataset)
     assert N % batch_size == 0, 'N should be divisible by batch_size'
     num_batches = N // batch_size
+
     for i in range(num_batches):
-        imgs_orig = []
-        labels = []
+        imgs_orig = dataset.data[batch_size * i:batch_size * (i + 1)].to(device).type(torch.float) / 255.
+        labels = dataset.targets[batch_size * i:batch_size * (i + 1)].to(device)
         # Create corruption masks
         masks = []
-        for j in range(batch_size*i, batch_size*(i + 1)):
-            img, label = dataset[j]
-            imgs_orig.append(img)
-            labels.append(label)
+        for _ in range(batch_size):
             # Choose square size
             s = rng.randint(7, 15)
             # Choose top-left corner position
             x = rng.randint(0, 29 - s)
             y = rng.randint(0, 29 - s)
-            mask = torch.zeros((1, 28, 28), dtype=torch.uint8)
+            mask = torch.zeros(imgs_orig.shape[1:], dtype=torch.uint8)
             # Set mask area
-            mask[:, y:y + s, x:x + s] = 1
+            mask[y:y + s, x:x + s] = 1
             masks.append(mask)
-        imgs_orig = torch.stack(imgs_orig).to(device)
-        labels = torch.as_tensor(labels, device=device)
         masks = torch.stack(masks).to(device)
 
+        # Add channel dimension
+        imgs_orig.unsqueeze_(1)
+        masks.unsqueeze_(1)
+
+        # Generate corrupted versions
         imgs_corrupted = imgs_orig.clone()
-        # Draw squares
         imgs_corrupted[masks] = 1.
 
         # Generate restored images
         model_device = next(model.parameters()).device
         imgs_restored = model(imgs_corrupted.to(model_device)).to(device)
 
-        predicted_labels_orig = classifier(_preprocess_for_classifier(imgs_orig)).argmax(dim=-1)
-        predicted_labels_restored = classifier(_preprocess_for_classifier(imgs_restored)).argmax(dim=-1)
-        # Calculate classifier score
+        predicted_labels_orig = classifier(_preprocess_for_classifier(imgs_orig)).argmax(dim=-1).type_as(labels)
+        predicted_labels_restored = classifier(_preprocess_for_classifier(imgs_restored)).argmax(dim=-1).type_as(labels)
+        # Calculate classifier score:
+        # baseline corresponds to the original samples which the classifier is able to correctly predict
         baseline = labels == predicted_labels_orig
-        correct = baseline == (labels == predicted_labels_restored)
+        # Since the classifier is NOT 100% accurate, we ignore the prediction results
+        # from the original samples which were misclassified by masking it using the baseline.
+        correct = (labels == predicted_labels_restored) & baseline
         baseline_score += int(baseline.sum())
         correct_score += int(correct.sum())
 
-        ssim_score += ssim(imgs_orig[~masks].squeeze().numpy(), imgs_restored[~masks].squeeze().numpy())
+        # Compute SSIM over the uncorrupted pixels
+        ssim_score += ssim(imgs_orig[~masks].cpu().numpy(), imgs_restored[~masks].cpu().numpy())
 
     classifier_score = correct_score / baseline_score
     ssim_score /= num_batches
 
     print('Classifier score: {:.2f}\nSSIM score: {:.2f}'.format(100 * classifier_score, 100 * ssim_score))
+
+
+if __name__ == '__main__':
+    from torchvision import datasets
+    data = datasets.MNIST('../data', train=False, download=True)
+    model = IdentityModel()
+    test_model(model, data, batch_size=100)
